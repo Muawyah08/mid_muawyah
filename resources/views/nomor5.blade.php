2e979b0427037603f3a8deac369c18e2e03a2c41
LAPTOP-K9DLCFRB\ASUS ROG <60900119031@uin-alauddin.ac.id>
2021-11-09 11:12:40 -0800

    soal monor6

--- resources/views/nomor5.blade.php
+++ resources/views/nomor5.blade.php
@@ -8,5 +8,13 @@
 </head>
 <body>
     <p>muawyah</p>
+    <p>Version Control System Merupakan Suatu paket software dimana ketika kita meninisialisasi, VCS akan memonitor file kita untuk setiap perubahan dan mengizinkan kita untuk menandai perubahan tersebut pada setiap level perubahan , Sehingga kita dapat meninjau ulang tahapan yang ditandai kapan pun ketika dibutuhkan .</p>
+    <p>Mercurial</p>
+    <p>Monotone</p>
+    <p>Bazaar</p>
+    <p>TFS</p>
+    <p>VSTS</p>
+    <p>Perforce Helix Core</p>
+    <p>IBM Rational ClearCase</p>
 </body>
 </html>
